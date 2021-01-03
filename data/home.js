64fcd7b14b6b786d8ac94aed88f0c02db2994f29
Simon Andrews <simon@hubblehq.com>
2021-01-03 17:51:13 +0000

    feat: Use custom MDX components on HomeLayout
    Center headings, and specify Paragraph

---
+++ data/home.js
@@ -0,0 +1,120 @@
+import Celtx from "@/images/logo-celtx.svg"
+import EffortlessAdmin from "@/images/logo-effortless-admin.svg"
+import HubbleHQ from "@/images/logo-hubblehq.svg"
+import Neowin from "@/images/logo-neowin.svg"
+import ThomsonReuters from "@/images/logo-thomson-reuters.svg"
+import UniversityOfCalgary from "@/images/logo-university-of-calgary.svg"
+
+const home = {
+  education: [
+    {
+      company: "University of Calgary",
+      brandColor: "#E42124",
+      href: "https://ucalgary.ca",
+      isFocus: true,
+      logo: { type: "component", component: UniversityOfCalgary },
+      positions: [
+        {
+          title: "Bachelor of Science - Computer Science 2015",
+          time: "2011 - 2015",
+        },
+      ],
+    },
+  ],
+  work: [
+    {
+      company: "HubbleHQ",
+      brandColor: "#FF377A",
+      href: "https://hubblehq.com",
+      isFocus: true,
+      logo: { type: "component", component: HubbleHQ },
+      positions: [
+        {
+          title: "Engineering Manager",
+          time: "January 2020 - Current",
+        },
+        {
+          title: "Lead Product Engineer",
+          time: "July 2019 - January 2020",
+        },
+        {
+          title: "Product Engineer",
+          time: "March 2019 - July 2019",
+        },
+        {
+          title: "Product Engineer (contractor)",
+          time: "November 2018 - March 2019",
+        },
+      ],
+    },
+    {
+      company: "Thomson Reuters",
+      brandColor: "#E6772B",
+      href: "https://thomsonreuters.com",
+      isFocus: true,
+      logo: { type: "component", component: ThomsonReuters },
+      positions: [
+        {
+          title: "Web Developer (contractor)",
+          time: "March 2016 - October 2018",
+        },
+      ],
+    },
+    {
+      company: "Effortless Admin",
+      brandColor: "#1B9AD6",
+      href: "https://effortlessadmin.com",
+      logo: { type: "component", component: EffortlessAdmin },
+      positions: [
+        {
+          title: "Web Developer",
+          time: "May 2015 - March 2016",
+        },
+      ],
+    },
+    {
+      company: "Olympia Trust Company",
+      brandColor: "#AC8A2E",
+      href: "https://olympiatrust.com",
+      logo: {
+        type: "img",
+        imgAttributes: {
+          alt: "",
+          src: `/images/logo-olympia-trust-company.png`,
+        },
+      },
+      positions: [
+        {
+          title: ".NET Developer (intern)",
+          time: "Summers 2013 and 2014",
+        },
+      ],
+    },
+    {
+      company: "Celtx",
+      brandColor: "#000000",
+      href: "https://www.celtx.com",
+      logo: { type: "component", component: Celtx },
+      positions: [
+        {
+          title: "Developer (intern)",
+          time: "Summers 2008, 2010, 2011 and 2012",
+        },
+      ],
+    },
+    {
+      company: "Neowin.net",
+      brandColor: "#1E5078",
+      href: "https://neowin.net",
+      logo: { type: "component", component: Neowin },
+      positions: [
+        {
+          title: "Developer (part time)",
+          time: "December 2007 - January 2011",
+        },
+      ],
+    },
+  ],
+}
+
+export default home
--- layouts/HomeLayout.js
+++ layouts/HomeLayout.js
@@ -1,53 +1,65 @@
+import { MDXProvider } from "@mdx-js/react"
 import React from "react"
 import PropTypes from "prop-types"
 import Image from "next/image"
 
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
+import Heading from "@/components/Heading"
 import Hero from "@/components/Hero"
 import Paragraph from "@/components/Paragraph"
 import Section from "@/components/Section"
 
-import home from "@/data/home.json"
+import home from "@/data/home"
 import site from "@/data/site"
 
 import { content, wrapper } from "./Layout.module.css"
 
-const Layout = ({ children, heroContent }) => {
-  return (
-    <div className={wrapper}>
-      {/* Add a target for the Return To Top link */}
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid, jsx-a11y/anchor-is-valid, jsx-a11y/anchor-has-content */}
-      <a id="top" />
+const components = {
+  h1: Heading.Centered.H1,
+  h2: Heading.Centered.H2,
+  h3: Heading.Centered.H3,
+  h4: Heading.Centered.H4,
+  p: Paragraph,
+}
 
-      <Hero
-        background={
-          <Image layout="fill" objectFit="cover" src={home.heroImage} />
-        }
-      >
-        <Header
-          siteTitle={site.title}
-          siteLinks={site.siteLinks}
+const Layout = ({ children, heroContent, meta }) => {
+  return (
+    <MDXProvider components={components}>
+      <div className={wrapper}>
+        {/* Add a target for the Return To Top link */}
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid, jsx-a11y/anchor-is-valid, jsx-a11y/anchor-has-content */}
+        <a id="top" />
+
+        <Hero
+          background={
+            <Image layout="fill" objectFit="cover" src={meta.heroImage} />
+          }
+        >
+          <Header
+            siteTitle={site.title}
+            siteLinks={site.siteLinks}
+            socialLinks={site.socialLinks}
+          />
+          {heroContent}
+        </Hero>
+
+        <main className={content}>
+          <Section verticalPadding="xxl">{children}</Section>
+        </main>
+
+        <Footer
+          contactInfo={
+            <Paragraph>
+              You can get in touch with me at{" "}
+              <a href="mailto:hello@sadl.io">hello@sadl.io</a>, or via my social
+              media profiles. I am not currently looking for new work.
+            </Paragraph>
+          }
           socialLinks={site.socialLinks}
         />
-        {heroContent}
-      </Hero>
-
-      <main className={content}>
-        <Section verticalPadding="lg">{children}</Section>
-      </main>
-
-      <Footer
-        contactInfo={
-          <Paragraph>
-            You can get in touch with me at{" "}
-            <a href="mailto:hello@sadl.io">hello@sadl.io</a>, or via my social
-            media profiles. I am not currently looking for new work.
-          </Paragraph>
-        }
-        socialLinks={site.socialLinks}
-      />
-    </div>
+      </div>
+    </MDXProvider>
   )
 }
 
