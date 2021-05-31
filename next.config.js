dee8a12a9c3ee6bec1d2858334069746f88198b8
Simon Andrews <me@simonandrews.ca>
2021-05-31 15:26:16 +0100

    fix: Set locales

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const withSvgr = require("next-svgr")
 
 module.exports = withSvgr(
   withMDX({
+    i18n: {
+      defaultLocale: "en-GB",
+      locales: ["en", "en-CA", "en-GB", "en-US"],
+    },
     pageExtensions: ["js", "jsx", "mdx"],
   })
 )
